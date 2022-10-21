bcc3e360180a0cf049ab008b260644f907ed0717
Udit <uauditarora16@gmail.com>
2022-10-21 19:03:46 +0530

    final commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cout << "Enter the First number" << endl;
+  cin >> a;
+  cout << "Enter the Second number" << endl;
+  cin >> b;
+  if (a==b) {
+    cout << "Equal" << endl;
+  }else{
+    cout << "Not Equal" << endl;
+  }
+  return 0;
+}
